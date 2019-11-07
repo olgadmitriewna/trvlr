66d005057632455a24c6dd0f925af6158a680c8f
Jeyasreetha PArameswaran <jeyasreetha@googlemail.com>
2019-11-07 18:30:07 +0800

    add a link to each phrases to be added to the profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_034641) do
+ActiveRecord::Schema.define(version: 2019_11_07_095153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,15 @@ ActiveRecord::Schema.define(version: 2019_11_07_034641) do
     t.index ["country_id"], name: "index_places_on_country_id"
   end
 
+  create_table "user_phrases", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "phrase_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phrase_id"], name: "index_user_phrases_on_phrase_id"
+    t.index ["user_id"], name: "index_user_phrases_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -176,6 +185,8 @@ ActiveRecord::Schema.define(version: 2019_11_07_034641) do
   add_foreign_key "phrases", "vocabularies"
   add_foreign_key "pins", "places"
   add_foreign_key "places", "countries"
+  add_foreign_key "user_phrases", "phrases"
+  add_foreign_key "user_phrases", "users"
   add_foreign_key "vaccines", "healths"
   add_foreign_key "visa_types", "visas"
   add_foreign_key "visas", "countries"
