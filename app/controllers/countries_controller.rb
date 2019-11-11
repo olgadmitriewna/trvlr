5ac81526c3f03804693f9a6bc0b17b80478e83b7
Olga Voronina <o.d.vrnn@gmail.com>
2019-11-11 18:02:23 +0800

    created visa pages with the api  and did the css

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class CountriesController < ApplicationController
   def index
     @users = User.all
@@ -39,6 +42,19 @@ class CountriesController < ApplicationController
     # end
   end
 
+  def visa_info
+     @country = Country.find(params[:country_id])
+    if params[:citizen_country].present?
+      citizen_country = params[:citizen_country]
+      country_code = @country.code
+      @visa_info = get_visa_info(citizen_country, country_code)
+    end
+    respond_to do |format|
+      format.html { render 'show', locals: { visa_info:  @visa_info  }}
+      format.js
+    end
+  end
+
   def place
     @places = Place.geocoded # returns places with coordinates
 
@@ -48,5 +64,46 @@ class CountriesController < ApplicationController
         lng: place.longitude
       }
     end
+  end
+
+  def get_visa_info(citizen_country, country_code)
+    url = "http://requirements-api.sandbox.joinsherpa.com/v2/entry-requirements?citizenship=#{citizen_country}&destination=#{country_code}&language=en&key=AIzaSyACsHgC04uwmmgLYEJzQy53ME1pS_ujFMA"
+    response = open(url).read
+    datas = JSON.parse(response)
+
+    # visa
+    warning = datas["visa"].first["textual"]["class"] # if class=warning red font else black
+    visa_required = datas["visa"].first["textual"]["text"][0] # you need a visa
+    allowed_stay = datas["visa"].first["allowed_stay"] # 30 days
+    type = datas["visa"].first["type"] # tourist
+
+    requirement = datas["visa"].first["requirement"] # do an if statement if NOT_REQUIRED puts "Not required" else puts "Required" end
+
+    info1 = datas["visa"].first["textual"]["text"][1] # "You may be eligible for an eVisa for your trip."
+    info2 = datas["visa"].first["textual"]["text"][2] # "An eVisa is delivered electronically, and can be obtained online before your trip to Vietnam."
+
+    # passport
+    blank_pages = datas["passport"]["textual"]["blank_pages"]["text"][1] # Your passport must have one blank page.
+    valid = datas["passport"]["textual"]["passport_validity"]["text"][1] # Your passport must be valid for six months at the time you enter Vietnam."
+
+    # vaccination
+
+    # currency
+    currency_arrival = datas["currency"]["arrival"]
+    currency_exit = datas["currency"]["exit"]
+
+    { warning: warning,
+      visa_required: visa_required,
+      allowed_stay: allowed_stay,
+      type: type,
+      requirement: requirement,
+      info1: info1,
+      info2: info2,
+      blank_pages: blank_pages,
+      valid: valid,
+      recom_vaccination: datas["vaccination"]["recommended"],
+      risk_vaccination: datas["vaccination"]["risk"],
+      currency_arrival: currency_arrival,
+      currency_exit: currency_exit }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_034918) do
+ActiveRecord::Schema.define(version: 2019_11_11_021411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_034918) do
     t.float "latitude"
     t.string "language"
     t.string "photo"
+    t.string "code"
   end
 
   create_table "culture_tips", force: :cascade do |t|
