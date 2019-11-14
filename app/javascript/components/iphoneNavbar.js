9c8648a9866814184d9ebcbe3aaad1b3c4b7d505
Elise Wetherholt <ewether@gmail.com>
2019-11-14 17:04:38 +0800

    fixed safari

--- app/javascript/components/iphoneNavbar.js
+++ app/javascript/components/iphoneNavbar.js
@@ -1,13 +1,21 @@
 const burgerMenu = document.querySelector(".navbar-toggler");
-var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+var iOS = !!navigator.platform && /iPad|iPhone|iPod/i.test(navigator.platform);
+var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
 const banner = document.querySelector(".banner")
+const countryBanner = document.querySelector(".country-banner")
+const nav = document.querySelector(".navbar")
 
 const toggleIphoneNavbar = () => {
-  if (burgerMenu && iOS) {
+  if (burgerMenu && (iOS || isSafari) && banner) {
+    nav.style.padding = "30px";
     burgerMenu.addEventListener('click', (event) => {
       banner.classList.toggle("iphone")
     })
+  } else if (countryBanner && (iOS || isSafari) && burgerMenu) {
+    nav.style.padding = "30px";
+    burgerMenu.addEventListener('click', (event) => {
+      countryBanner.classList.toggle("iphone")
+    })
   }
 }
-console.log('called iphone')
 export { toggleIphoneNavbar }
